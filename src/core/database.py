c44c5a937215badb88045ce8e25ae9ae88fd2cd4
Sal <salvadoraleguas@gmail.com>
2022-12-01 22:39:20 -0500

    Update database.py

--- src/core/database.py
+++ src/core/database.py
@@ -43,4 +43,36 @@ logging.info("Loading database module")
 # ║               ║          ║          ║
 # ╚═══════════════╩══════════╩══════════╝
 
-class AnalyticsDbHandler:
+class AnalyticsDBHandler:
+    """Class to handle all the data analytics, especially stuff like creating tables, making backups, etc."""
+
+     def __init__(self) -> None:
+        pass
+
+    def create_songs_table(self) -> bool:
+        """Create the songs table, returns True if successful, False if not."""
+        pass
+
+    def create_plays_table(self) -> bool:
+        """Create the plays table, returns True if successful, False if not."""
+        pass
+
+    def create_playlists_table(self) -> bool:
+        """Create the playlists table, returns True if successful, False if not."""
+        pass
+
+    def create_playlists_songs_table(self) -> bool:
+        """Create the playlists_songs table, returns True if successful, False if not."""
+        pass
+
+    def create_all_tables(self) -> bool:
+        """Create all the tables, returns True if successful, False if not."""
+        pass
+
+    def backup_database(self) -> bool:
+        """Backup the database, returns True if successful, False if not."""
+        pass
+
+    def restore_database(self) -> bool:
+        """Restore the database, returns True if successful, False if not."""
+        pass 
