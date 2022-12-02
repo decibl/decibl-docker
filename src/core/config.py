b53d8e734ec485301eb2ab35fcdc469cd45b3815
Sal <salvadoraleguas@gmail.com>
2022-12-01 22:33:37 -0500

    Updated logger

--- src/core/config.py
+++ src/core/config.py
@@ -2,7 +2,7 @@
 from enum import Enum
 import os, sys, logging
 from logging.handlers import TimedRotatingFileHandler
-
+import datetime
 
 class Config(Enum):
     # Logging
@@ -10,7 +10,8 @@ class Config(Enum):
     # For logging format, do datetime + file location then message
     LOGGING_FORMAT = "%(asctime)s - %(pathname)s - %(levelname)s - %(message)s"
     LOGGING_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-    LOGGING_FILENAME = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "logs", "example.log"))
+    # make filename based on date
+    LOGGING_FILENAME = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "logs", "log_{}.log".format(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S"))))
     LOGGING_ENCODING = "utf-8"
 
 # set logging config
