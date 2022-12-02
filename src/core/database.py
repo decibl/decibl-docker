4a86f3392f76e55210665ddfa1c702b92f9102de
Sal <salvadoraleguas@gmail.com>
2022-12-01 22:31:32 -0500

    Update database.py

--- src/core/database.py
+++ src/core/database.py
@@ -3,9 +3,42 @@ import os, sys, logging
 # add current file to system path
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
-# import config.py
-
 import config
 
 # log a message
-logging.info("Loading database module")
+logging.info("Loading database module...")
+
+
+# Make Database class to hold all the data analytics
+# This will be used to create and manage the database of the users activity.
+# (there's lots of data in the songs table so I'm not going to put it in the graphic)
+# ╔══════════════════════════════════════════════════════╗
+# ║                         SONGS                        ║
+# ╠═══════════╦══════════╦═══════╦════════╦═══════╦══════╣
+# ║ **SONG_ID ║ FILEPATH ║ TITLE ║ ARTIST ║ ALBUM ║ ETC. ║
+# ║           ║          ║       ║        ║       ║      ║
+# ║           ║          ║       ║        ║       ║      ║
+# ╚═══════════╩══════════╩═══════╩════════╩═══════╩══════╝
+# ╔════════════════════════════════════════════╗
+# ║                    PLAYS                   ║
+# ╠═══════════╦════════════╦══════════╦════════╣
+# ║ **PLAY_ID ║ SONG_TITLE ║ START_DT ║ END_DT ║
+# ║           ║            ║          ║        ║
+# ║           ║            ║          ║        ║
+# ╚═══════════╩════════════╩══════════╩════════╝
+# ╔════════════════════════════════════════════╗
+# ║                  PLAYLISTS                 ║
+# ╠═══════════════╦═══════════════╦════════════╣
+# ║ **PLAYLIST_ID ║ PLAYLIST_NAME ║ CREATED_DT ║
+# ║               ║               ║            ║
+# ║               ║               ║            ║
+# ║               ║               ║            ║
+# ╚═══════════════╩═══════════════╩════════════╝
+# ╔═════════════════════════════════════╗
+# ║           PLAYLISTS_SONGS           ║
+# ╠═══════════════╦══════════╦══════════╣
+# ║ **PLAYLIST_ID ║ *SONG_ID ║ ADDED_DT ║
+# ║               ║          ║          ║
+# ║               ║          ║          ║
+# ║               ║          ║          ║
+# ╚═══════════════╩══════════╩══════════╝
