d2671e0838f3b6b479e20f7b10251feaa9f2a754
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2022-12-11 13:20:08 -0500

    boom

---
+++ src/tests/test_songparser.py
@@ -0,0 +1,25 @@
+import pytest
+import sys
+import os
+import sqlite3
+import random
+import zipfile
+
+# add the parent directory to the path
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "..", "core")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "..", "backups")))
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "..", "databases")))
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "..", "soundfiles")))
+
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "testsongs")))
+    
+import config
+import analyticsdb
+import songparser
+
