eea99c916fbd03f61a93494709f43a17b5112d96
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2022-12-11 13:55:04 -0500

    Update test_songparser.py

--- src/tests/test_songparser.py
+++ src/tests/test_songparser.py
@@ -18,8 +18,16 @@ sys.path.append(os.path.abspath(os.path.join(
 
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), "testsongs")))
-    
+
 import config
 import analyticsdb
 import songparser
+
+
+def test_file_to_hash():
+    filepath = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), "testsongs", "02 - Gemstone.flac"))
+    hash = songparser.file_to_hash(filepath)
+    assert hash == "23fb2258052511a4d07bc555a1b45a41fbd8da0f3ec4a887c9c7282351672956"
+
 
